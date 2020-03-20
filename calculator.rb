080a4c7620b5ca7ba270120a615c4989183ed23f
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-20 18:17:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,6 @@ first_number = 5
 second_number = 10
 
 sum = first_number + second_number
-difference = second_number - first_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = second_number / first_number
